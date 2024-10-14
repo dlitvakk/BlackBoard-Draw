84b8510ae75edaf44e9cc420797d6953f06457c4
Dana Litvak <dlitvak@kse.org.ua>
2024-10-14 18:54:04 +0300

    added fill / frame mode

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <utility>
 #include <vector>
 #include <fstream>
 
@@ -87,14 +88,15 @@ public:
 
     int id;
     std::string type;
+    std::string mode, color;
     std::vector<int> info;
     std::vector<std::vector <char>>& grid;
 
 public:
     virtual ~Figure() = default;
 
-    Figure(int id, std::string type, const std::vector<int> &info, std::vector<std::vector <char>>& grid)
-        : id(id), type(std::move(type)), info(info), grid(grid) {}
+    Figure(int id, std::string type, std::string mode, std::string color, const std::vector<int> &info, std::vector<std::vector <char>>& grid)
+        : id(id), type(std::move(type)), mode(std::move(mode)), color(std::move(color)), info(info), grid(grid) {}
 
     virtual void draw() {
         std::cout << "Drawing non-existing shape!" << std::endl;
@@ -103,7 +105,7 @@ public:
         std::cout << "  > ^ <\n";
     }
 
-    void getInfo() const{
+    void getInfo() const { // !!!!!!!!
         std::cout  << id << " " << type;
         for (int i = 0; i < info.size(); ++i) {
             if (i == 0) {
@@ -121,9 +123,10 @@ public:
 class Triangle : public Figure {
 public:
     ~Triangle() override = default;
-    Triangle(int id, const std::vector<int> &info, std::vector<std::vector <char>>& grid)
-        : Figure(id, "triangle", info, grid) {}
-    void draw() override {
+    Triangle(int id, std::string mode, std::string color, const std::vector<int> &info, std::vector<std::vector <char>>& grid)
+        : Figure(id, "triangle", std::move(mode), std::move(color), info, grid) {}
+
+    void drawFrame () const {
         int x = info[0], y = info[1], height = info[2];
         if (height <= 0) return;
         for (int i = 0; i < height; ++i) {
@@ -148,14 +151,42 @@ public:
                 grid[baseY][baseX] = '*';
         }
     }
+
+    void drawFill() const {
+        int x = info[0], y = info[1], height = info[2];
+        if (height <= 0) return;
+        for (int i = 0; i < height; ++i) {
+            int leftMost = x - i;
+            int rightMost = x + i;
+            int posY = y + i;
+
+            if (posY < BOARD_HEIGHT) {
+                for (int j = leftMost; j <= rightMost; ++j) {
+                    if (j >= 0 && j < BOARD_WIDTH) {
+                        grid[posY][j] = '*';
+                    }
+                }
+            }
+        }
+    }
+    void draw() override {
+        if (mode == "fill") {
+            drawFill();
+        } else if (mode == "frame") {
+            drawFrame();
+        } else {
+            std::cout << "Invalid mode!" << std::endl;
+        }
+    }
 };
 
 class Rhombus : public Figure {
 public:
     ~Rhombus() override = default;
-    Rhombus(int id, const std::vector<int> &info, std::vector<std::vector <char>>& grid)
-        : Figure(id, "rhombus", info, grid) {}
-    void draw() override {
+    Rhombus(int id, std::string mode, std::string color, const std::vector<int> &info, std::vector<std::vector <char>>& grid)
+        : Figure(id, "rhombus", std::move(mode), std::move(color), info, grid) {}
+
+    void drawFrame() const {
         int x = info[0], y = info[1], height = info[2];
         if (height <= 0) return;
         if (height % 2 != 0) {
@@ -189,14 +220,59 @@ public:
             }
         }
     }
+    void drawFill() const {
+        int x = info[0], y = info[1], height = info[2];
+        if (height <= 0) return;
+        if (height % 2 != 0) {
+            std::cout << "Please, enter valid height for rhombus. Height should be even number." << std::endl;
+            return;
+        }
+        for (int i = 0; i <= height / 2; ++i) {
+            int leftMost = x - i;
+            int rightMost = x + i;
+            int posY = y + i;
+
+            if (posY < BOARD_HEIGHT) {
+                for (int j = leftMost; j <= rightMost; ++j) {
+                    if (j >= 0 && j < BOARD_WIDTH) {
+                        grid[posY][j] = '*';
+                    }
+                }
+            }
+        }
+
+        for (int i = height / 2 - 1; i >= 0; --i) {
+            int leftMost = x - i;
+            int rightMost = x + i;
+            int posY = y + height - i;
+
+            if (posY < BOARD_HEIGHT) {
+                for (int j = leftMost; j <= rightMost; ++j) {
+                    if (j >= 0 && j < BOARD_WIDTH) {
+                        grid[posY][j] = '*';
+                    }
+                }
+            }
+        }
+    }
+    void draw() override {
+        if (mode == "fill") {
+            drawFill();
+        } else if (mode == "frame") {
+            drawFrame();
+        } else {
+            std::cout << "Invalid mode!" << std::endl;
+        }
+    }
 };
 
 class Circle : public Figure {
 public:
     ~Circle() override = default;
-    Circle(int id, const std::vector<int> &info, std::vector<std::vector <char>>& grid)
-        : Figure(id, "circle", info, grid) {}
-    void draw() override {
+    Circle(int id, std::string mode, std::string color, const std::vector<int> &info, std::vector<std::vector <char>>& grid)
+        : Figure(id, "circle", std::move(mode), std::move(color), info, grid) {}
+
+    void drawFrame() const {
         int x = info[0], y = info[1], radius = info[2];
         if (radius <= 0) return;
         for (int i = -radius; i <= radius; ++i) {
@@ -216,14 +292,45 @@ public:
             }
         }
     }
+
+    void drawFill() const {
+        int x = info[0], y = info[1], radius = info[2];
+        if (radius <= 0) return;
+        for (int i = -radius; i <= radius; ++i) {
+            int posY = y + i + radius;
+
+            int offset = int(sqrt(radius * radius - i * i));
+            int posX1 = x + offset;
+            int posX2 = x - offset;
+
+            if (posY >= 0 && posY < BOARD_HEIGHT) {
+                for (int j = posX2; j <= posX1; ++j) {
+                    if (j >= 0 && j < BOARD_WIDTH) {
+                        grid[posY][j] = '*';
+                    }
+                }
+            }
+        }
+    }
+
+    void draw() override {
+        if (mode == "fill") {
+            drawFill();
+        } else if (mode == "frame") {
+            drawFrame();
+        } else {
+            std::cout << "Invalid mode!" << std::endl;
+        }
+    }
 };
 
 class Rectangle : public Figure {
 public:
     ~Rectangle() override = default;
-    Rectangle(int id, const std::vector<int> &info, std::vector<std::vector <char>>& grid)
-        : Figure(id, "rectangle", info, grid) {}
-    void draw() override {
+    Rectangle(int id, std::string mode, std::string color, const std::vector<int> &info, std::vector<std::vector <char>>& grid)
+        : Figure(id, "rectangle", std::move(mode), std::move(color), info, grid) {}
+
+    void drawFrame() const {
         int x = info[0], y = info[1], width = info[2], height = info[3];
         if (width <= 0 || height <= 0) return;
 
@@ -251,6 +358,31 @@ public:
             }
         }
     }
+
+    void drawFill() const {
+        int x = info[0], y = info[1], width = info[2], height = info[3];
+        if (width <= 0 || height <= 0) return;
+
+        for (int i = 0; i < width; ++i) {
+            int posX = x + i;
+            if (posX >= 0 && posX < BOARD_WIDTH) {
+                for (int j = y; j < y + height; ++j) {
+                    if (j >= 0 && j < BOARD_HEIGHT) {
+                        grid[j][posX] = '*';
+                    }
+                }
+            }
+        }
+    }
+    void draw() override {
+        if (mode == "fill") {
+            drawFill();
+        } else if (mode == "frame") {
+            drawFrame();
+        } else {
+            std::cout << "Invalid mode!" << std::endl;
+        }
+    }
 };
 
 class CommandParser {
@@ -271,18 +403,18 @@ public:
         std::cout << "rectangle <x> <y> <width> <height>" << std::endl;
     }
 
-     void add(const std::string &type, const std::vector<int>& info) const {
+     void add(const std::string &type, const std::string &mode, const std::string &color, const std::vector<int>& info) const {
         if (type == "triangle") {
-            std::unique_ptr<Triangle> tr = std::make_unique<Triangle>(figures.size() + 1, info, board.grid);
+            std::unique_ptr<Triangle> tr = std::make_unique<Triangle>(figures.size() + 1, mode, color, info, board.grid);
             figures.push_back(std::move(tr));
         } else if (type == "rhombus") {
-            std::unique_ptr<Rhombus> r = std::make_unique<Rhombus>(figures.size() + 1, info, board.grid);
+            std::unique_ptr<Rhombus> r = std::make_unique<Rhombus>(figures.size() + 1, mode, color, info, board.grid);
             figures.push_back(std::move(r));
         } else if (type == "circle") {
-            std::unique_ptr<Circle> c = std::make_unique<Circle>(figures.size() + 1, info, board.grid);
+            std::unique_ptr<Circle> c = std::make_unique<Circle>(figures.size() + 1, mode, color, info, board.grid);
             figures.push_back(std::move(c));
         } else if (type == "rectangle") {
-            std::unique_ptr<Rectangle> rec = std::make_unique<Rectangle>(figures.size() + 1, info, board.grid);
+            std::unique_ptr<Rectangle> rec = std::make_unique<Rectangle>(figures.size() + 1, mode, color, info, board.grid);
             figures.push_back(std::move(rec));
         } else {
             std::cout << "Invalid shape type!" << std::endl;
@@ -340,6 +472,8 @@ public:
 
             std::string type;
             std::cin >> type;
+            std::string mode, color;
+            std::cin >> mode >> color;
             std::vector<int> info;
             if (type == "rectangle") {
                 for (int i = 0; i < 4; ++i) {
@@ -355,11 +489,12 @@ public:
 
                 }
             }
-
             if (validation(type, info) == true) {
-                add(type, info);
+                std::cout << "\033[32m";
+                add(type, mode, color, info);
+                std::cout << "\033[0m";
                 std::cout << "Shape is succesfully added." << std::endl;
-            };
+            }
         } else if (command == "clear") {
             figures.clear();
             board.clear();
